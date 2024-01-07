fe1740043510cc1f7271400b4efbfd5f6926634a
UMANG1233 <lit2019033@iiitl.ac.in>
2024-01-07 03:04:03 -0500

    Changed routes in server folder

--- server/routes/testRoutes.js
+++ server/routes/testRoutes.js
@@ -4,28 +4,32 @@ const router = express.Router();
 const { VERIFY_EMAIL } = require("../config/constants");
 const { sendMail } = require("../services/emailService");
 
+// Route for testing
 router.get(
   "/test",
   asyncHandler(async (req, res) => {
-    res.status(200).json({ sucess: "Hello world!!" });
+    res.status(200).json({ success: "Hello world!!" });
   })
 );
 
+// Route for sending verification email
 router.post(
   "/email",
   asyncHandler(async (req, res) => {
+    // Extract user information from the request body
     const { user } = req.body;
+
+    // Send a verification email using the email service
     await sendMail(user, VERIFY_EMAIL)
       .then(() =>
         res.status(200).json({ message: `Email sent to ${user.email}` })
       )
       .catch((err) => {
-        res
-          .status(400)
-          .json({
-            message: `Error sending email to ${user.email}`,
-            error: err,
-          });
+        // Handle errors during the email sending process
+        res.status(400).json({
+          message: `Error sending email to ${user.email}`,
+          error: err,
+        });
       });
   })
 );
