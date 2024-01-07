ce6d5f753613c825c772926bc9e82a42375b00fa
UMANG1233 <lit2019033@iiitl.ac.in>
2024-01-07 00:34:13 -0500

    Changed db in server

--- server/config/db.js
+++ server/config/db.js
@@ -2,14 +2,18 @@ const mongoose = require("mongoose");
 
 const connectDb = async () => {
   try {
+    // Connect to MongoDB using the provided URI from the environment variables
     const conn = await mongoose.connect(process.env.MONGO_URI);
-    console.log(`Mongo DB connected: ${conn.connection.host}`);
+    
+    // Log a successful connection message
+    console.log(`MongoDB connected: ${conn.connection.host}`);
   } catch (error) {
-    console.log(
-      `Unable to connect with Mongo DB: ${error.message}`.cyan.underline
-    );
+    // Log an error message and exit the process if unable to connect
+    console.error(`Unable to connect to MongoDB: ${error.message}`.red.underline);
     process.exit(1);
   }
 };
 
+// Export the connectDb function to be used in other files
 module.exports = connectDb;
+
