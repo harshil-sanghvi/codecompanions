aece44251795f6b1c3754573d873821ea2f707dc
Harshil Snehalkumar  Sanghvi <harshil.sanghvi@stonybrook.edu>
2024-01-07 19:37:15 -0500

    Modified footer

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,29 +1,43 @@
 import React from "react";
-import { AiFillLinkedin, AiOutlineGithub } from "react-icons/ai";
+import { AiOutlineGithub } from "react-icons/ai";
 
 const Footer = () => {
   return (
-    <div className="w-full p-[2px] mt-auto flex items-center gap-4 justify-center mx-0 bottom-5 text-center z-50 bg-zinc-800 text-white">
-      <h1 className="text-sm tracking-wide">
-        Made with <span>&#10084;</span> by Umang Goenka and Harshil Sanghvi
-        <span></span>
-      </h1>
-
-      {/* <a
-        href="https://www.linkedin.com"
-        rel="noreferrer"
-        target="_blank"
-      >
-        <AiFillLinkedin className="cursor-pointer" />
-      </a> */}
-
-      <a
-        href="https://github.com/harshil-sanghvi/codecompanions"
-        rel="noreferrer"
-        target="_blank"
-      >
-        <AiOutlineGithub className="cursor-pointer" />
-      </a>
+    <div className="w-full p-[2px] mt-auto flex flex-col items-center gap-2 justify-center mx-0 bottom-5 text-center z-50 bg-[#431407] text-white h-20">
+      <div>
+        <span>
+          Made by{" "}
+          <a
+            href="https://www.linkedin.com/in/umang-goenka-1622b01a3/"
+            rel="noreferrer"
+            target="_blank"
+            className="text-blue-400 hover:underline"
+          >
+            Umang Goenka
+          </a>{" "}
+          and{" "}
+          <a
+            href="https://www.linkedin.com/in/harshilsanghvi/"
+            rel="noreferrer"
+            target="_blank"
+            className="text-blue-400 hover:underline"
+          >
+            Harshil Sanghvi
+          </a>
+        </span>
+      </div>
+      <div>
+        Check out the code on{" "}
+        <a
+          href="https://github.com/harshil-sanghvi/codecompanions"
+          rel="noreferrer"
+          target="_blank"
+          className="text-blue-400 hover:underline"
+        >
+          GitHub
+          <AiOutlineGithub className="cursor-pointer inline-block ml-1" />
+        </a>
+      </div>
     </div>
   );
 };
